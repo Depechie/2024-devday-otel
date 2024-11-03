6033eeffb2014720b3fdd717ce2141a33196c932
Glenn Versweyveld <glenn.versweyveld@gmail.com>
2024-11-03 15:47:57 +0100

    Add RabbitMQ - part 1

--- aspireotel.AppHost/Program.cs
+++ aspireotel.AppHost/Program.cs
@@ -1,9 +1,19 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+var messaging = builder.AddRabbitMQ("messaging")
+    .WithManagementPlugin()
+    .PublishAsContainer();
+
 var apiService = builder.AddProject<Projects.aspireotel_ApiService>("apiservice");
+
+var serviceWorker = builder.AddProject<Projects.aspireotel_WorkerService>("workerservice")
+    .WithReference(messaging)
+    .WaitFor(messaging);
 
 builder.AddProject<Projects.aspireotel_Web>("webfrontend")
     .WithExternalHttpEndpoints()
-    .WithReference(apiService);
+    .WithReference(apiService)
+    .WithReference(messaging)
+    .WaitFor(messaging);
 
 builder.Build().Run();
